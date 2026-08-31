6b9792aaf11092dc29f4d1de52b6694bbc4226f8
felipeaugdev <felipeaugusto12161@gmail.com>
2026-08-30 23:52:27 -0300

    refactor: convert ExpenseRepository to Spring Data JPA repository

--- src/main/java/com/felipeaugdev/ExpenseRepository.java
+++ src/main/java/com/felipeaugdev/ExpenseRepository.java
@@ -1,29 +1,9 @@
 package com.felipeaugdev;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ExpenseRepository {
-
-    /**
-     * Loads all persisted expenses from storage.
-     * 
-     * @return List of Expense objects
-     */
-    List<Expense> loadExpenses();
-
-    /**
-     * Persists or updates a single expense in storage.
-     * 
-     * @param expenses The individual expense object to save.
-     */
-    void save(Expense expenses);
-
-    /**
-     * Deletes a single expense from storage by its unique ID
-     * 
-     * @param id The unique ID of the expense to remove
-     * @return true if the expense was found and deleted, false otherwise
-     */
-    boolean deleteById(int id);
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 
 }
